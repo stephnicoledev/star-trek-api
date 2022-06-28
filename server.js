d8242a6cf457ca55a7e23a88253bc5fcf5a8a6bb
iamstephs <stranosteph@gmail.com>
2022-06-28 19:06:30 -0400

    set up server

--- server.js
+++ server.js
@@ -4,3 +4,22 @@ const cors = require(cors);
 const PORT = 8000;
 
 app.use(cors());
+
+const aliens = {};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:alienName", (req, res) => {
+  const aliensName = req.params.alienName.toLowerCase();
+  if (aliens[aliensName]) {
+    res.json(aliens[aliensName]);
+  } else {
+    res.json(aliens["humans"]);
+  }
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log("Server is running!");
+});
